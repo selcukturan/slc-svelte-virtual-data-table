1e69f4dcc380501de3d38bebcdb6b274a2914f1c
Selcuk TURAN <selcuk.turann@outlook.com>
2024-11-28 20:22:51 +0300

    Deployment test.

--- src/lib/website/slc/components/template/Sidebar.svelte
+++ src/lib/website/slc/components/template/Sidebar.svelte
@@ -7,13 +7,13 @@
 	<div class="flex h-full w-full flex-col border border-border/70">
 		<div class="hidden">h</div>
 		<div class="flex flex-1 flex-col gap-2 overflow-hidden overflow-y-auto p-4">
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 1</div>
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 2</div>
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 3</div>
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 4</div>
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 5</div>
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 6</div>
-			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 7</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 8</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 9</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 10</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 11</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 12</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 13</div>
+			<div class="rounded-sm bg-amber-200 p-2 dark:bg-amber-700/20">test 14</div>
 		</div>
 		<div class="hidden">f</div>
 	</div>
