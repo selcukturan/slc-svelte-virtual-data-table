6de4956bc5e6258e6bf62cebbbcd31d29dac11fb
Selcuk TURAN <selcuk.turann@outlook.com>
2024-11-29 18:38:19 +0300

    Website sayfa yapısı tasarlandı.

--- src/lib/website/slc/components/template/Footer.svelte
+++ src/lib/website/slc/components/template/Footer.svelte
@@ -6,5 +6,5 @@
 	class="border-t border-border/70 bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
 	<!-- {@render children?.()} -->
-	(footer)
+	(v.0.0.1)
 </footer>
